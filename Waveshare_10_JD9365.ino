0ff2079d1de3541c90f99894965bba85dcce5862
Nigel Morton <nigel@ntpworld.co.uk>
2026-01-13 06:18:21 +0100

    Add project files.

---
+++ Waveshare_10_JD9365.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Waveshare_10_JD9365.ino
+ Created:	1/13/2026 6:16:18 AM
+ Author:	nigel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
